83c3117cc03097267978895dd4df17c86ed02255
bin549 <__2018bb>
2020-12-04 10:28:07 +0800

    1

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def main():
+    # read file
+    # get column
+    # draw simple
+    # sentence
+    # draw it
+    pass
+
+
+if __name__ == '__main__':
+    main()
